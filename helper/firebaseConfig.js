886c1d145875682f0fe3d84e9d3b466a54c7b394
Giuseppe <giuseppspathis@gmail.com>
2025-11-16 23:33:52 +0100

    added the firebaseConfig

---
+++ helper/firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDEGmR667fB6Q9R3IGh3_bAvhHjw3ORJms",
+  authDomain: "my-arcade-game.firebaseapp.com",
+  projectId: "my-arcade-game",
+  storageBucket: "my-arcade-game.firebasestorage.app",
+  messagingSenderId: "668361154030",
+  appId: "1:668361154030:web:d1c286151228c93f8802ff",
+  measurementId: "G-5NKJLCP850"
+};
